c23aaf99ce93c8be4eefd87f036b8417433948ca
Alvn <alvnmugo@gmail.com>
2024-08-14 16:15:22 +0300

    Fix 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ApplyForm from './components/ApplyForm';
+
+function App() {
+  return (
+        <div>
+           <ApplyForm/>
+        </div>
+ );
+}
+
+export default App;
