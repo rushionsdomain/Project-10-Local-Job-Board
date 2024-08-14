53b6b1640b31fafbcee11c3f0a295178d046a532
Alvn <alvnmugo@gmail.com>
2024-08-14 16:11:45 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-import Navbar from './components/Navbar';
 import ApplyForm from './components/ApplyForm';
 function App() {
   return (
         <div>
-           {/* <Navbar /> */}
            <ApplyForm/>
         </div>
 
