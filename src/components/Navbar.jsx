c0b4a54e702f7ec085d4287211129e5101713401
Alvn <alvnmugo@gmail.com>
2024-08-14 16:01:44 +0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 const Navbar = () => {
   return (
-    <nav>
-      <h2>Local Job Board</h2>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/signin">Sign In</Link></li>
-        <li><Link to="/register">Register</Link></li>
-        <li><Link to="/"/></li>
-      </ul>
-    </nav>
+    <div>
+      <h1>net-working</h1>
+    </div>
   );
 };
 
 export default Navbar;
-daaaaaaaaaaaaaa
