4ca599054e3e3fd7c23dddb61a102f12c1ecb99d
kwesbian <kwechristian@gmail.com>
2024-08-14 15:32:10 +0300

    Started HomePage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './components/HomePage';
 import Footer from './components/Footer';
+
 
 function App() {
   return (
     <>
+      <HomePage/>
       <Footer/>
     </>
   );
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from './Navbar';
+
+function HomePage() {
+    return (
+        <div className="HomePage">
+            <h2>Welcome to Net-Work</h2>
+            <p>Connecting local businesses and job seekers in your community.</p>
+            <p>Browse job listings and post your openings today</p>
+        </div>
+    );
+}
+
+export default HomePage;
