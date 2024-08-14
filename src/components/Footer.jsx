688d203609d87556800f69ee578f66373c039a06
kwesbian <kwechristian@gmail.com>
2024-08-14 12:57:15 +0300

    Updated Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import './footer.css';
 
 function Footer() {
+    const [selectedLanguage, setSelectedLanguage] = useState('en');
+
+    const handleLanguageChange = (event) => {
+        setSelectedLanguage(event.target.value);
+    };
+
     return (
-        <footer className="footer">
+        <footer>
+            <div className="footer-content">
             <p>&copy; 2024 Net-Work. All Rights Reserved.</p>
+            <div className="language-dropdown">
+                <select value={selectedLanguage} onChange={handleLanguageChange}>
+                    <option value="en">English</option>
+                    <option value="es">Español</option>
+                    <option value="fr">Français</option>
+                    <option value="de">Deutsch</option>
+                    <option value="sw">Swahili</option>
+                </select>
+            </div>
+            </div>    
         </footer>
     );
 }
