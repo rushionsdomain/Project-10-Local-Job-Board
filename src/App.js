ee7556b899cbfaec1a6ff833bb60d30841bd99eb
kwesbian <kwechristian@gmail.com>
2024-08-14 22:16:56 +0300

    fix 1

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/HomePage';
 import Footer from './components/Footer';
-import SignInForm from './components/SignInForm';
-import RegisterForm from './components/RegisterForm';
+
 
 function App() {
   return (
     <>
     <HomePage />
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/" element={<SignInForm />} />
-        <Route path="/" element={<RegisterForm />} />
-      </Routes>
-    </Router>
     <Footer />
     </>
   );
