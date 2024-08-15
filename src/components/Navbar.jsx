bd7294fe4b84815809e01432ada254d5ff2f5533
Alvn <alvnmugo@gmail.com>
2024-08-15 09:46:32 +0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-
-const Navbar = () => {
+function Navbar() {
   return (
-    <div>
-      <h1>net-working</h1>
-    </div>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <a href="/">MyLogo</a>
+      </div>
+      <ul className="navbar-links">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/services">Services</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
   );
-};
+}
 
 export default Navbar;
-z
