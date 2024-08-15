d37e784ade739bc005da7949c84af2c257afa55c
Alvn <alvnmugo@gmail.com>
2024-08-15 09:34:52 +0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-const Navbar = () => {
+function Navbar() {
   return (
     <div>
       <h1>net-working</h1>
     </div>
   );
-};
+}
 
 export default Navbar;
